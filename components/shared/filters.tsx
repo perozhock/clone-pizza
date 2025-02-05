1c9734b9282b29a2611b55c344e9a4f6c8ae7648
perozhock <shat.prog1@gmail.com>
2025-02-06 02:08:45 +0300

    add and render new component

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FilterCheckbox, RangeSlider, Title } from ".";
+import { CheckboxFiltersGroup, FilterCheckbox, RangeSlider, Title } from ".";
 import { Input } from "../ui";
 
 interface Props {
@@ -19,12 +19,81 @@ export const Filters: React.FC<Props> = ({ className }) => {
             <div className="mt-5 border-y-neutral-100 py-6 pb-7">
                 <p className="font-bold mb-3">Цена от и до:</p>
                 <div className="flex gap-3 mb-5">
-                    <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
-                    <Input type="number" min={100} max={1000} placeholder="1000" />
+                    <Input
+                        type="number"
+                        placeholder="0"
+                        min={0}
+                        max={1000}
+                        defaultValue={0}
+                    />
+                    <Input
+                        type="number"
+                        min={100}
+                        max={1000}
+                        placeholder="1000"
+                    />
                 </div>
 
                 <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
             </div>
+
+            <CheckboxFiltersGroup
+                title="Ингридиенты"
+                className="mt-5"
+                limit={5}
+                defaultItems={[
+                    {
+                        text: "Совас",
+                        value: "3",
+                    },
+                    {
+                        text: "Моцареловое",
+                        value: "4",
+                    },
+                    {
+                        text: "Чесноковое",
+                        value: "5",
+                    },
+                    {
+                        text: "Огурковое",
+                        value: "6",
+                    },
+                    {
+                        text: "Луковое",
+                        value: "7",
+                    },
+                    {
+                        text: "Помидоровое",
+                        value: "8",
+                    },
+                ]}
+                items={[
+                    {
+                        text: "Совас",
+                        value: "3",
+                    },
+                    {
+                        text: "Моцареловое",
+                        value: "4",
+                    },
+                    {
+                        text: "Чесноковое",
+                        value: "5",
+                    },
+                    {
+                        text: "Огурковое",
+                        value: "6",
+                    },
+                    {
+                        text: "Луковое",
+                        value: "76",
+                    },
+                    {
+                        text: "Помидоровое",
+                        value: "7",
+                    },
+                ]}
+            />
         </div>
     );
 };
