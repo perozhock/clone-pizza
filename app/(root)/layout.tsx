44d5c7504147389b5ee5d84d7dea64d41c7612a8
perozhock <shat.prog1@gmail.com>
2025-03-20 00:33:22 +0300

    move header to a separate layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Header } from "@/components/shared";
+
+export const metadata: Metadata = {
+    title: "Clone Pizza | Главная",
+    icons: {
+        icon: "/logo.png",
+    },
+};
+
+export default function HomeLayout({
+    children,
+    modal,
+}: Readonly<{
+    children: React.ReactNode;
+    modal: React.ReactNode;
+}>) {
+    return (
+        <main className="min-h-screen">
+            <Header />
+            {children}
+            {modal}
+        </main>
+    );
+}
